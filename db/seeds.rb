e6aeaea39e0445fcf72cbc75a77490197160407f
themonster2015 <quynhyen.vothi@gmail.com>
2021-11-09 23:46:14 +0100

    create greeting table and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Greeting.create(text: "Hello")
+Greeting.create(text: "Nihao")
+Greeting.create(text: "Xin chao")
+Greeting.create(text: "Bonjour")
+Greeting.create(text: "Guten Tag")
